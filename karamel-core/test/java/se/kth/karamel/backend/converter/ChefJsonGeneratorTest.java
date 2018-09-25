3fe75a30035fd8ab5e6b9fc4248a86822cbd5113
Fabio Buso <buso.fabio@gmail.com>
2018-09-25 22:55:59 +0200

    tests should not be under src

--- karamel-core/test/java/se/kth/karamel/backend/converter/ChefJsonGeneratorTest.java
+++ karamel-core/test/java/se/kth/karamel/backend/converter/ChefJsonGeneratorTest.java
@@ -58,6 +58,7 @@ public class ChefJsonGeneratorTest {
     Assert.assertNotNull(chefJsons.get("mgmnodes1ndb::purge"));
     JsonObject jsonObject = chefJsons.get("mgmnodes1ndb::purge");
     String st = jsonObject.toString();
+    System.out.println(st);
     Assert.assertTrue(st.contains("\"DataMemory\":\"111\""));
   }
 
